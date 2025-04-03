dd271d726c8a025649e16f3c044f8c2f7af69b0f
nikitakonan <nikitakonan1991@gmail.com>
2025-04-03 13:32:33 +0400

    Added users table and integrate with user service

--- app/src/users/services/users.service.ts
+++ app/src/users/services/users.service.ts
@@ -1,30 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { randomUUID } from 'node:crypto';
+import { Injectable, Logger } from '@nestjs/common';
 import { User } from '../models';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+function mapUserEntityToUser({ email, ...rest }: UserEntity): User {
+  return { email: email || undefined, ...rest };
+}
 
 @Injectable()
 export class UsersService {
-  private readonly users: Record<string, User>;
+  private readonly logger = new Logger(UsersService.name);
 
-  constructor() {
-    this.users = {};
-  }
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
 
-  findOne(name: string): User {
-    for (const id in this.users) {
-      if (this.users[id].name === name) {
-        return this.users[id];
-      }
+  async findOne(name: string): Promise<User | undefined> {
+    this.logger.log(`findOne: ${name}`);
+    const userEntity = await this.userRepository.findOne({ where: { name } });
+    if (userEntity) {
+      return mapUserEntityToUser(userEntity);
     }
     return;
   }
 
-  createOne({ name, password }: User): User {
-    const id = randomUUID();
-    const newUser = { id, name, password };
-
-    this.users[id] = newUser;
-
-    return newUser;
+  async createOne({ name, password }: User): Promise<User> {
+    this.logger.log(`createOne: ${name}`);
+    const userEntity = await this.userRepository.save({ name, password });
+    this.logger.log(`created: ${userEntity.id}`);
+    return mapUserEntityToUser(userEntity);
   }
 }
